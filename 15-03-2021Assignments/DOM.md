f3593948218eaa624847bedf9fbe444cd09751db
olaifa olawale <olaifaolawaleh@gmail.com>
2021-03-16 07:15:22 +0100

    Objects, Arrrays and DOM

---
+++ 15-03-2021Assignments/DOM.md
@@ -0,0 +1,15 @@
+# JAVASCRIPT DOM
+**DOM stands for Document Object Module.**     
+The **Document Object Module** is an interface that defines the logical structure of and the way a document is accessed and manipulated. 
+
+With the *document object module*, programmers can *build documents, navigate their structure, and add, modify, or delete elements and contents.* Anything found in an HTML or XML docment can be accessed, changed, deleted or added using the document object module, with a few exceptions.
+
+With javascript DOM, we can perform multiple tasks, create new elements and attributes, change the existing elements and attributes and even remove existing elements and attributes. Javascript an also react to existing events and create new event in the page.
+
+##  Few Examples of Methods used to manipulate DOM using Javascript:
++ ***getElementById();*** - used to access elements and attributes whose id is set.
++ ***innerHTML***: used to access the content of an element.
++ ***document.querySelector();*** - this can be used to pick any element on our HTML page. this can be either a class, an id or any tag.
++ ***document.createElement();*** - this creates a new element.
++ ***textContent*** - this is used to specify or set the HTML contents inside the element.
+    > *this is just a few of many Javascript DOM methods.*
---
+++ 15-03-2021Assignments/Objects&Arrays.js
@@ -0,0 +1,88 @@
+const country = {
+    name: 'Nigeria',
+    capital: 'Abuja',
+    president: 'Muhammadu Buhari',
+    continent: 'Africa',
+    continentalRegion: 'West Africa',
+    landMass: '923 768 square kilometers',
+    population: '201 000 000',
+    states: 36,
+    formOfGovernment: 'Democratic',
+    yearOfIndependence: 1960
+}
+
+const state = {
+    name: 'Lagos',
+    capital: 'Ikeja',
+    country: 'Nigeria',
+    geoPoliticalZone: 'SouthWest',
+    governor: 'Babajide SanwoOlu',
+    deputyGovernor: 'Femi Hamzat',
+    population: '14 368 000',
+    landMass: '1171 square kilometres',
+    termLength: '4 years',
+    numberOfLocalGovernments: 37,
+}
+
+const car = {
+    name: 'Audi',
+    model: 'A3 Sportback',
+    generations: 9,
+    firstProductionYear: 1999,
+    bodyStyle: 'Hatchback',
+    color: 'Blue',
+    engine: 'Gasoline',
+    topSpeed: '139mph (224km/h)',
+    fuelCapacity: '50L',
+    engineSpecs: {
+        cylinders: 'L4',
+        displacement: '1498 cubic centimetres',
+        power: '150 HP @ 5000-6000 RPM',
+        torque: '250Nm @ 1500-3500 RPM',
+        fuelSystem: 'Turbocharged Direct Injection'
+
+    } 
+
+}
+
+const namesOfMyPets = [
+    ['Skuppy', 'Bingo', 'Sentri'],
+    ['Meowzer', 'Frankie', 'Bowser'],
+    ['Simba', 'Mufasa', 'Scar']
+]
+
+const footballClubs = [
+    ['Kano pillars', 'Sunshine stars', 'MFM FC'],
+    ['Chelsea', 'Liverpool', 'Manchester city'],
+    ['Barcelona FC', 'Real madrid', 'Barcelona', 'Athletico Madrid']
+]
+
+const food = [
+    ['Amala', 'Semo', 'Pounded yam'],
+    ['Fried rice', 'jollof rice', 'coconut rice'],
+    ['meat', 'fish', 'egg']
+]
+
+const countries = [
+    {
+        name: 'Ghana',
+        continent: 'Africa',
+        continentalRegion: 'West Africa',
+        yearOfIndependence: '1957',
+        capital: 'Accra',
+        population: '30 420 000',
+        landMass: '288 535 square kilometres',
+        currency: 'Naira'
+    },
+
+    {
+        name: 'Germany',
+        continent: 'Europe',
+        continentalRegion: 'Central Europe',
+        yearOfIndependence: '1871',
+        capital: 'Berlin',
+        population: '83 783 942',
+        landMass: '357 021 square kilometres',
+        currency: 'Euro'
+    }
+]
